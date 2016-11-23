e4c379655cdf2d6224d6a2e1fda8a7a787f38454
Owain Kenway <o.kenway@ucl.ac.uk>
2016-11-23 15:42:44 +0000

    Go implementation.

---
+++ go/src/mandel.go
@@ -0,0 +1,49 @@
+package main
+
+import ("pnmmodules")
+
+func main() {
+    var pixels[][] int
+    var xmin, xmax, ymin, ymax, rmax, x0, y0, x, y, xt float64
+    var xres, yres, max_iter, iter, px, py int
+
+    xmax = 2.0
+    xmin = -2.0
+
+    ymax = 2.0
+    ymin = -2.0
+
+    xres = 2048
+    yres = 2048
+
+    max_iter = 256
+
+    rmax = 2.0
+
+    pixels = make([][]int, xres)
+    for px = 0; px < xres; px++ {
+        pixels[px] = make([]int, yres)
+
+        x0 = ((float64(px)/float64(xres)) * float64(xmax - xmin)) + xmin
+
+        for py = 0; py < yres; py++ {
+            y0 = ((float64(py)/float64(yres)) * float64(ymax - ymin)) + ymin
+
+            iter = 0
+            x = 0.0
+            y = 0.0
+
+            for (((x*x + y*y) < (rmax * rmax)) && (iter < max_iter)) {
+                xt = (x*x) - (y*y) + x0
+                y = 2.0 * x * y + y0
+                x = xt
+                iter = iter + 1                
+            }
+            pixels[px][py] = max_iter - iter
+        }
+
+    } 
+    pnmmodules.Writepgm(pixels, max_iter - 1, "mandel.pgm")
+
+
+}
