f603fe30c4f9d4e25318e73178067eca06002446
Owain Kenway <o.kenway@ucl.ac.uk>
2016-11-28 13:09:24 +0000

    Benchmark data.

---
+++ bench/plot.R
@@ -0,0 +1,16 @@
+# Plot timing data for different mandel implementations
+
+# Read our data
+timings <- read.csv("timing.csv")
+
+# Sort ascending by time
+sortedtimings <- timings[order(timings$Time),]
+
+# Open our output file
+png(file="timing.png", width=720, height=720)
+
+# Plot
+barplot(sortedtimings$Time, names.arg=sortedtimings$Language, xlab="Language", ylab="Run time (seconds)", main="Run time of mandel code by language")
+
+# Tidy up
+dev.off()
