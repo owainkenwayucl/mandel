4ba75138247ce1fb5dbdbe687a221824c954bccc
Owain Kenway <o.kenway@ucl.ac.uk>
2016-12-07 10:23:37 +0000

    Julia implementation.

---
+++ julia/mandel.jl
@@ -0,0 +1,43 @@
+include("imageio.jl")
+import imageio
+
+xmax = 2.0
+xmin = -2.0
+
+ymax = 2.0
+ymin = -2.0
+
+xres = 2048
+yres = 2048
+
+max_iter = 256
+
+rmax = 2.0
+
+pixels = zeros(Int64, xres, yres)
+
+for px = 1:xres
+  x0 = (Float64(px)/Float64(xres)) * Float64(xmax - xmin) + xmin
+
+  for py = 1:xres
+    y0 = (Float64(py)/Float64(yres)) * Float64(ymax - ymin) + ymin
+
+    iter = 0
+
+    x = 0.0
+    y = 0.0
+
+    while (((x*x + y*y) < (rmax * rmax)) && (iter < max_iter)) 
+      xt = (x*x) - (y*y) + x0
+      y = 2.0 * x * y + y0
+      x = xt
+      iter = iter + 1
+    end
+
+    pixels[px,py] = max_iter - iter
+  end
+ 
+
+end
+
+imageio.writepgm(pixels, max_iter - 1, "mandel.pgm")
