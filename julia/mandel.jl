3eb8031c37e3804dd24c08d14eea6848d264be1e
Owain Kenway <o.kenway@ucl.ac.uk>
2016-12-09 13:49:43 +0000

    Split mandel() into a function.
    This means it gets compiled, drastically improving speed.

--- julia/mandel.jl
+++ julia/mandel.jl
@@ -1,43 +1,38 @@
 include("imageio.jl")
 import imageio
 
-xmax = 2.0
-xmin = -2.0
+# Split out mandel as a function so we can call it if need be.
+function mandel(xmin=-2.0, xmax=2.0, ymin=-2.0, ymax=2.0, max_iter=256, xres=2048, yres=2048, filename="mandel.pgm")
 
-ymax = 2.0
-ymin = -2.0
+  rmax = 2.0
+  pixels = zeros(Int64, xres, yres)
 
-xres = 2048
-yres = 2048
+  for px = 1:xres
+    x0 = (Float64(px)/Float64(xres)) * Float64(xmax - xmin) + xmin
 
-max_iter = 256
+    for py = 1:xres
+      y0 = (Float64(py)/Float64(yres)) * Float64(ymax - ymin) + ymin
 
-rmax = 2.0
+      iter = 0
 
-pixels = zeros(Int64, xres, yres)
+      x = 0.0
+      y = 0.0
 
-for px = 1:xres
-  x0 = (Float64(px)/Float64(xres)) * Float64(xmax - xmin) + xmin
+      while (((x*x + y*y) < (rmax * rmax)) && (iter < max_iter)) 
+        xt = (x*x) - (y*y) + x0
+        y = 2.0 * x * y + y0
+        x = xt
+        iter = iter + 1
+      end
 
-  for py = 1:xres
-    y0 = (Float64(py)/Float64(yres)) * Float64(ymax - ymin) + ymin
-
-    iter = 0
-
-    x = 0.0
-    y = 0.0
-
-    while (((x*x + y*y) < (rmax * rmax)) && (iter < max_iter)) 
-      xt = (x*x) - (y*y) + x0
-      y = 2.0 * x * y + y0
-      x = xt
-      iter = iter + 1
+      pixels[px,py] = max_iter - iter
     end
-
-    pixels[px,py] = max_iter - iter
-  end
  
+
+  end
+
+  imageio.writepgm(pixels, max_iter - 1, "mandel.pgm")
 
 end
 
-imageio.writepgm(pixels, max_iter - 1, "mandel.pgm")
+mandel()
